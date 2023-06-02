391eebd1c1f53905686d0d2ed53785077ddb1023
JustCodeIt22 <justcodeit22@gmail.com>
2023-06-02 09:48:03 +0530

    Added the navigation using mouse

--- TextEditor.py
+++ TextEditor.py
@@ -219,9 +219,17 @@ class TextEditor:
 
         # Mouse Events
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                self.update_cursor_according_to_mouse()
-
+            match(event.button):
+                case 1: # Left Mouse Button
+                    self.update_cursor_according_to_mouse()
+                case 4: # Scroll Up
+                    print("Scroll Up")
+                    self.cursor_surf_rect.y += self.font_size[1]
+                case 5: # Scroll Down
+                    print("Scroll Down")
+                    self.cursor_surf_rect.y -= self.font_size[1]
+            
+                
 
     # Get the current column and row
     def get_row_n_col(self, relative_pos):
@@ -251,15 +259,24 @@ class TextEditor:
             self.cursor_surf = pygame.Surface((4,self.font_size[1] + 2))
     
 
-    # update cursor pos
+    # update cursor pos according to mouse pos
     def update_cursor_according_to_mouse(self):
         self.mouse_pos = pygame.mouse.get_pos()
-        row = round(self.mouse_pos[1] / self.font_size[1])
-        col = round(self.mouse_pos[0] / self.font_size[0])
-        self.cursor_surf_rect.x = col * self.font_size[0]
-        self.cursor_surf_rect.y = row * self.font_size[1]
-        self.line_num = row - 1
-        print(row, col)
+        self.mouse_pos = (self.mouse_pos[0] - self.te_pos[0], self.mouse_pos[1] - self.te_pos[1])
+        row = self.mouse_pos[1] // self.font_size[1]
+        col = self.mouse_pos[0] // self.font_size[0]
+        if row >= 1 and col >= 4:
+            if (0 <= row - 1 < len(self.text)):
+                if col - 4 <= len(self.text[row - 1]):
+                    self.curr_col = col - 4
+                    self.cursor_surf_rect.x = col * self.font_size[0] + 4
+                    self.cursor_surf_rect.y = row * self.font_size[1]
+                    self.line_num = row - 1
+                else:
+                    self.curr_col = col - 4
+                    self.line_num = row - 1
+                    self.cursor_surf_rect.x = (len(self.text[self.line_num]) + 4) * self.font_size[0] + 4
+                    self.cursor_surf_rect.y = row * self.font_size[1]
 
 
     # update cursor pos according to up/down/left/right keys
